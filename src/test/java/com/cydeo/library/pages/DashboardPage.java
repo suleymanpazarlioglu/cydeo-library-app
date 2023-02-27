b0622af168be5477ce5bc95db15191c903069b54
spazarlioglu <105013545+spazarlioglu@users.noreply.github.com>
2023-02-27 22:25:48 +0300

    dashboard page added

--- src/test/java/com/cydeo/library/pages/DashboardPage.java
+++ src/test/java/com/cydeo/library/pages/DashboardPage.java
@@ -1,4 +1,14 @@
 package com.cydeo.library.pages;
 
+import com.cydeo.library.utilities.Driver;
+import org.openqa.selenium.support.PageFactory;
+
 public class DashboardPage {
+
+    public DashboardPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+
+
 }
